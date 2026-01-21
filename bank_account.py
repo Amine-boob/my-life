f9b722e21eb912870020899e9e50704f29c880a6
Amine-boob <ghommit2004@gmail.com>
2026-01-21 21:22:38 +0100

    Implement bank account class with deposit and withdraw

--- bank_account.py
+++ bank_account.py
@@ -47,3 +47,75 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+# this time working with class 
+
+class Banc :
+    def __init__(self):
+        self.balance = 0
+
+    def show_balance(self):
+        print(f"you have in your account ${self.balance:.2f}")
+
+    def deposit(self):
+        amount = input("enter an amount to deposited")
+        if amount.isdigit():
+            amount = float(amount)
+            if amount <= 0:
+                print("you can't deposit negative number or zero 📛")
+
+            else :
+                 self.balance += amount
+        else:
+            print("enter just digits 📛")
+
+    def withdraw(self):
+        amount = input("enter a number to drow :")
+        if amount.isdigit():
+            amount = float(amount)
+            if amount > self.balance:
+                print("you don't have this amount 📛")
+
+            elif amount <= 0:
+                print("you can't withdraw a negative number or zero 📛")
+
+            else:
+                self.balance -= amount
+        else:
+            print("enter just digits 📛")
+
+
+def main():
+    user = Banc()
+    is_running = True
+    while is_running :
+        print("-------------")
+        print("bank program ")
+        print("-------------")
+        print("1.to show balance")
+        print("2.to deposit an amount ")
+        print("3.with draw ")
+        print("4.quit")
+        print("-------------")
+        choice = input("enter your choice (1-4) :")
+        if choice == "1":
+            user.show_balance()
+        elif choice == "2":
+            user.deposit()
+        elif choice == "3":
+            user.withdraw()
+        elif choice == "4":
+            is_running = False
+        else:
+            print("your choice invalid !! 📛")
+    print("have a good day")
+
+if __name__ == '__main__':
+    main()
